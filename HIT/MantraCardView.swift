354d80ed1fab54ef2d6fb2df76fe5c9e3c290cc8
nmelehan <nmelehan@gmail.com>
2015-12-07 11:16:39 -0500

    Created class for mantra's card view

---
+++ HIT/MantraCardView.swift
@@ -0,0 +1,29 @@
+//
+//  MantraCardView.swift
+//  HIT
+//
+//  Created by Nathan Melehan on 12/7/15.
+//  Copyright © 2015 Nathan Melehan. All rights reserved.
+//
+
+import UIKit
+
+class MantraCardView: UIView {
+    
+    //
+    //
+    //
+    //
+    // MARK: - Properties
+    
+    var mantra: String?
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
